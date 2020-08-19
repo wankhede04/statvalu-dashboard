f67b97567f79b40a39f085a4fcd804437b7e4738
wankhede04 <wankhedevijay04@gmail.com>
2020-08-19 08:50:32 +0530

    Added components configuration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShellComponent } from './shell/shell.component';
+import { ExcelComponent } from './excel/excel.component';
+import { DocsComponent } from './docs/docs.component';
+import { PdfsComponent } from './pdfs/pdfs.component';
+import { ChartsComponent } from './charts/charts.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent,
+    ExcelComponent,
+    DocsComponent,
+    PdfsComponent,
+    ChartsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
