1086b1eadd04f9af27a13d824722d5e5ba693305
wankhede04 <wankhedevijay04@gmail.com>
2020-08-19 08:49:24 +0530

    Added routing to functional component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
+import { ExcelComponent } from './excel/excel.component';
+import { DocsComponent } from './docs/docs.component';
+import { PdfsComponent } from './pdfs/pdfs.component';
+import { ChartsComponent } from './charts/charts.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    component: ShellComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'excel', component: ExcelComponent },
+      { path: 'docs', component: DocsComponent },
+      { path: 'pdfs', component: PdfsComponent },
+      { path: 'charts', component: ChartsComponent },
+    ],
+    data: { reuse: true }
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
